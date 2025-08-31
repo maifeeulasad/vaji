6384f70e4658bc515cd0e053e0550320b5d9dec8
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-08-31 15:36:52 +0600

    [up]: disabled links for images;

--- example/vite/react/src/App.tsx
+++ example/vite/react/src/App.tsx
@@ -10,12 +10,12 @@ function App() {
   const [showGuide, setShowGuide] = useState(true);
   return <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        {/* <a href="https://vite.dev" target="_blank"> */}
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {/* </a> */}
+        {/* <a href="https://react.dev" target="_blank"> */}
+          <img src="/vaji/src/assets/cloud-fog-svgrepo-com.svg" className="logo react" alt="React logo" />
+        {/* </a> */}
       </div>
       
       {/* Editable heading */}
