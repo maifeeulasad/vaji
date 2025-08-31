743fe463b0913aaf53e953d84ce51f18f9545130
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-08-31 13:53:32 +0600

    [example]: using deps from npm;

--- example/vite/react/vite.config.ts
+++ example/vite/react/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import viteReactLiveEditor from 'vaji'
+import viteReactLiveEditor from 'vaji/dist/vite-react-live-editor'
 
 // https://vite.dev/config/
 export default defineConfig({
